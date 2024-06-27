cabb1fec9cd252f9f1dfcf44401c1363d88a1f0b
Bishenp241999 <praptibishen1999@gmail.com>
2024-06-27 23:29:50 +0530

    Second commit - Accordion using HTML/CSS/JS

--- HTMLCSSJS/accordionDemo/script.js
+++ HTMLCSSJS/accordionDemo/script.js
@@ -1,9 +1,15 @@
-// USE OF EVENT DELEGATION 
-const parentEl = document.querySelector(".accordion");
-parentEl.addEventListener("click", function (e) {
-  const clicked = e.target.closest('.item');
-  console.log(clicked);
+// // USE OF EVENT DELEGATION 
+// const container = document.querySelector(".accordion");
+// container.addEventListener("click", function (e) {
+//   const clicked = e.target.closest('.item').querySelector('.hidden-box');
+//   console.log(clicked);
+// });
 
-  clicked.classList.toggle('open');
-  
-});
+const icons = document.querySelectorAll('.icon');
+icons.forEach(icon => {
+  icon.addEventListener('click',function(e){
+    const item = icon.parentNode;
+    item.classList.toggle('open')
+    console.log(item);
+  })
+})
